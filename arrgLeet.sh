f98132a24de38bfebcf54fdf8d5a90b14944b53d
lxyu0405 <xiaoyu.lu0405@gmail.com>
2016-03-18 12:46:13 +0800

    a shell file enable my quicker leetcode arrangement

---
+++ arrgLeet.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ $# != 2 ]
+then
+  echo "**Format** ./arrgLeet.sh [file_name] [file_type(py/c/cpp/java/js)]"
+
+else
+  if test ${2} == "py" -o ${2} == "c" -o ${2} == "cpp" -o ${2} == "java" -o ${2} == "js"; then
+    echo "Get the file_type: $2"
+    echo "Creating the directory: $1"
+    cd ~/Documents/MyMacWorkSpace/leetcode-pratice/ 
+    mkdir ${1}
+    cd ${1}
+    vi Solution.${2}
+
+  else
+    echo "Unvalid file_type(py/c/cpp/java/js)"
+    echo "**Format** ./arrgLeet.sh [file_name] [file_type(py/c/cpp/java/js)]"
+
+  fi
+fi
