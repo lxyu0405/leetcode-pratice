b68bd178b9b166a69c3d6fc249fa03791ff226cb
lxyu0405 <xiaoyu.lu0405@gmail.com>
2016-04-16 16:04:44 +0800

    Add SQL format file to Shell

--- arrgLeet.sh
+++ arrgLeet.sh
@@ -2,10 +2,10 @@
 
 if [ $# != 2 ]
 then
-  echo "**Format** ./arrgLeet.sh [file_name] [file_type(py/c/cpp/java/js)]"
+  echo "**Format** ./arrgLeet.sh [file_name] [file_type(py/c/cpp/java/js/sql)]"
 
 else
-  if test ${2} == "py" -o ${2} == "c" -o ${2} == "cpp" -o ${2} == "java" -o ${2} == "js"; then
+  if test ${2} == "py" -o ${2} == "c" -o ${2} == "cpp" -o ${2} == "java" -o ${2} == "js" -o ${2} == "sql"; then
     echo "Get the file_type: $2"
     echo "Creating the directory: $1"
     cd ~/Documents/MyMacWorkSpace/leetcode-pratice/ 
