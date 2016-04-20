4bd20c8ce760ba56d9bb2e0464c601fbe4d153b5
lxyu0405 <xiaoyu.lu0405@gmail.com>
2016-04-20 13:01:26 +0800

    Daily Leetcode
    1. Summary Ranges, two pointers.
    2. Reverse Integer, overflow, long -> int
    3. Reverse Bits, no unsigned int in java, can be replaced with long
    when representing long bits number.
    4. Integer Break, DP:
    for (int j = 1; j <= i/2; j++){
      int pro1 = max(j, maxProduct[j]);
      int pro2 = max(i-j, maxProduct[i-j]);
      maxProduct[i] = max(maxProduct[i], pro1*pro2);
    }

---
+++ reverse_integer/Solution.java
@@ -0,0 +1,15 @@
+public class ReverseInteger {
+    public int reverse(int x) {
+        int flag = x > 0 ? 1 : -1;
+        int num = x > 0 ? x : -x;
+        long  res = 0;
+        while (num > 0){
+            res = res * 10 + num % 10;
+            num /= 10;
+            if (res < Integer.MIN_VALUE || res > Integer.MAX_VALUE){
+                return 0;
+            }
+        }
+        return (int)res * flag;
+    }
+}
