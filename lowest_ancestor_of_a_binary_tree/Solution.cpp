2d5f326afbd09bdb11be7646cb9a9f73ce6a7225
lxyu0405 <xiaoyu.lu0405@gmail.com>
2016-06-29 12:12:50 +0800

    Lowest Ancestor of a Binary Tree
    if (leftSearch && rightSearch)      return root;
            if (!leftSearch && !rightSearch)    return NULL;
            if (!leftSearch)                    return rightSearch;
            return leftSearch;

---
+++ lowest_ancestor_of_a_binary_tree/Solution.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == NULL || root == p || root == q) {
+            return root;
+        }
+        TreeNode* leftSearch = lowestCommonAncestor(root->left, p, q);
+        TreeNode* rightSearch = lowestCommonAncestor(root->right, p, q);
+        
+        if (leftSearch && rightSearch)      return root;
+        if (!leftSearch && !rightSearch)    return NULL;
+        if (!leftSearch)                    return rightSearch;
+        return leftSearch;
+    }
+};
